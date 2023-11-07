d01df3ebf29bed8d4e6ad11e70a827641fbaad6d
Aya <90342033+Aya-X@users.noreply.github.com>
2023-11-07 16:43:05 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import reactLogo from './assets/react.svg';
-import './App.css';
+import './styles/App.css';
 
 function App(): JSX.Element {
   const [count, setCount] = useState<number>(0);
